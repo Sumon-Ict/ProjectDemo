d82af900f6ab0582d93f59e4ad4fb034784113c6
sumon <sumoniceiu1415@gmail.com>
2024-05-12 14:31:17 +0600

    added ADOnet example

---
+++ ADONETE/ADONETE/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADONETE
+{
+    internal class Program
+    {
+        private const string Connection_String = "Server =DESKTOP-ORVIE5J\\SQLEXPRESS;Database=Csharp;User Id = sumon;Password=sumonict";
+
+        private static SqlConnection _sqlConnection;
+
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("helow word");
+         _sqlConnection = new SqlConnection(Connection_String);
+
+
+        }
+        public static void Write(string sql)
+        {
+            try
+            {
+                if (_sqlConnection.State != System.Data.ConnectionState.Open)
+
+                { _sqlConnection.Open(); }
+
+                var command = new SqlCommand(sql, _sqlConnection);
+                command.ExecuteNonQuery();
+
+            }
+
+
+            catch (SqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                _sqlConnection?.Close();
+            }
+
+
+        }   
+    }
+}
