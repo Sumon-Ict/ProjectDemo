434c24599fbe0d5f9893fa4130f1d2530db092a6
sumon <sumoniceiu1415@gmail.com>
2024-05-13 01:43:58 +0600

    added threading code

---
+++ Threading/PrintUsingThread.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Threading
+{
+    internal class PrintUsingThread
+    {
+
+        public void threadA()
+        {
+            for(var i = 0; i < 20;i++)
+            {
+                Console.WriteLine("printing from thread 1");
+                Thread.Sleep(300);
+
+            }
+        }
+        public void threadB()
+        {
+            for (var i = 0;i < 20;i++)
+            {
+                Console.WriteLine("printing from thread 2");
+                Thread.Sleep(300);
+            }
+           
+
+        }
+        public static void thread1()
+        {
+            for(var i=0;i < 20;i++)
+            {
+                Console.WriteLine("printing thread 1");
+                Thread.Sleep(500);
+            }
+        }
+        public static void thread2()
+        {
+            for(int  i = 0;i < 20; i++)
+            {
+                Console.WriteLine("printing thread 2");
+                Thread.Sleep(500);
+            }
+        }
+    }
+}
---
+++ Threading/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Threading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //with static method 
+               var a = new Thread(PrintUsingThread.thread1);
+               var b=new Thread(PrintUsingThread.thread2);
+               a.Start();
+               b.Start();
+
+            //without static method 
+            var ob = new PrintUsingThread();
+
+            var th1 = new Thread(new ThreadStart(ob.threadA));
+            var th2= new Thread(new ThreadStart(ob.threadB)); 
+            
+            th1.Start();
+            th2.Start();
+
+
+
+
+
+        }
+    }
+}
