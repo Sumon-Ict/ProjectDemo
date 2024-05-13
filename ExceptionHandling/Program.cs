4cee1cbe82a8edb3fe733939515a6d77b0dfe16e
sumon <sumoniceiu1415@gmail.com>
2024-05-13 21:52:03 +0600

    added exception handling code

---
+++ ExceptionHandling/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GetText();
+        }
+        static void GetText()
+        {
+            try
+            {
+                var path = "c//myfile";
+                var text = File.ReadAllText(path);
+                if(!string.IsNullOrEmpty(text))
+                {
+                    throw new EmptyFileException("file is empty");
+
+                }
+                Console.WriteLine(text);
+            }
+            catch(FileNotFoundException ex) {
+                Console.WriteLine(ex.Message);
+            }
+            catch(EmptyFileException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            catch(Exception ex)
+            {
+                Console.WriteLine("exception found   "+ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("program ended");
+            }
+
+        }
+    }
+}
