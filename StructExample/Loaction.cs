8e5fd6072a1fcf796d8d7981441a789759c381c9
sumon <sumoniceiu1415@gmail.com>
2024-05-13 14:46:58 +0600

    added strut  code

---
+++ StructExample/Loaction.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructExample
+{
+    internal struct Loaction
+    {
+        public double longitude;
+        public double latitude;
+
+        public Loaction(double longitude, double latitude)
+        {
+            this.longitude = longitude;
+            this.latitude = latitude;
+        }
+
+        public bool Equals(Loaction other)
+        {
+         return   other.longitude==longitude &&
+            other.latitude==latitude;
+        }
+        public bool Equals(double x,double y)
+        {
+            return longitude == x && latitude ==y;
+
+        }
+
+    }
+}
